9291ec2deba53d9ee31204eea3334361e62c16af
sergey <karakhanyan.s@mail.ru>
2015-07-01 15:30:37 +0400

    change logout button

--- application/views/login_page.php
+++ application/views/login_page.php
@@ -5,22 +5,7 @@ if(isset($_SESSION['login'])){
 else{
     include_once('header.php');
 ?>
-<!--<div class="container">-->
-<!--    <form action="--><?php //echo base_url(); ?><!--user/login" method="post" >-->
-<!--        <table>-->
-<!--            <tr><td><input name="login" type="text" placeholder="Login"/></td></tr>-->
-<!--            <tr><td><input name="password" type="password" placeholder="Password"/></td></tr>-->
-<!--            <tr><td><input type="submit" value="Log In"/></td></tr>-->
-<!--            --><?php
-//            if(isset($message)){
-//            ?>
-<!--            <tr><td>--><?php //echo $message ?><!--</td></tr>-->
-<!--            --><?php
-//            }
-//            ?>
-<!--        </table>-->
-<!--    </form>-->
-<!--</div>-->
+
     <div class="text-center" style="padding:50px 0">
         <div class="logo">login</div>
         <!-- Main Form -->
