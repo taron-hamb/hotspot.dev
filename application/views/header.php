403f9d5241cd8d2f20c4df1a9c93d3d340a0e651
sergey <karakhanyan.s@mail.ru>
2015-07-01 15:01:15 +0400

    add login page css

--- application/views/header.php
+++ application/views/header.php
@@ -3,8 +3,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Website</title>
-    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="assets/css/custom.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/custom.css">
     <script type="text/javascript" href="assets/js/respond.min.js"></script>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.13.1/jquery.validate.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 </head>
 <body>
--- application/views/login_page.php
+++ application/views/login_page.php
@@ -5,22 +5,55 @@ if(isset($_SESSION['login'])){
 else{
     include_once('header.php');
 ?>
-<div class="container">
-    <form action="<?php echo base_url(); ?>user/login" method="post" >
-        <table>
-            <tr><td><input name="login" type="text" placeholder="Login"/></td></tr>
-            <tr><td><input name="password" type="password" placeholder="Password"/></td></tr>
-            <tr><td><input type="submit" value="Log In"/></td></tr>
-            <?php
-            if(isset($message)){
-            ?>
-            <tr><td><?php echo $message ?></td></tr>
-            <?php
-            }
-            ?>
-        </table>
-    </form>
-</div>
+<!--<div class="container">-->
+<!--    <form action="--><?php //echo base_url(); ?><!--user/login" method="post" >-->
+<!--        <table>-->
+<!--            <tr><td><input name="login" type="text" placeholder="Login"/></td></tr>-->
+<!--            <tr><td><input name="password" type="password" placeholder="Password"/></td></tr>-->
+<!--            <tr><td><input type="submit" value="Log In"/></td></tr>-->
+<!--            --><?php
+//            if(isset($message)){
+//            ?>
+<!--            <tr><td>--><?php //echo $message ?><!--</td></tr>-->
+<!--            --><?php
+//            }
+//            ?>
+<!--        </table>-->
+<!--    </form>-->
+<!--</div>-->
+    <div class="text-center" style="padding:50px 0">
+        <div class="logo">login</div>
+        <!-- Main Form -->
+        <div class="login-form-1">
+            <form id="login-form" action="<?php echo base_url(); ?>user/login" method="post" class="text-left">
+                <div class="login-form-main-message"></div>
+                <div class="main-login-form">
+                    <div class="login-group">
+                        <div class="form-group">
+                            <label for="lg_username" class="sr-only">Username</label>
+                            <input type="text" class="form-control" id="lg_username" name="login" placeholder="username">
+                        </div>
+                        <div class="form-group">
+                            <label for="lg_password" class="sr-only">Password</label>
+                            <input type="password" class="form-control" id="lg_password" name="password" placeholder="password">
+                        </div>
+
+                    </div>
+                    <button type="submit" class="login-button"><i class="fa fa-chevron-right"></i></button>
+                </div>
+                <div class="etc-login-form">
+                    <?php
+                    if(isset($message)){
+                        ?>
+                       <?php echo $message ?>
+                    <?php
+                    }
+                    ?>
+                </div>
+            </form>
+        </div>
+        <!-- end:Main Form -->
+    </div>
 <?php
     include_once('footer.php');
 }
